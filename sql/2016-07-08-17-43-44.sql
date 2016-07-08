c394b6c5d5b87c22afe7ccb4268cda5554ca0a97
jet <jetlyu521@yahoo.com>
2016-07-08 18:19:22 +0800

    first commit

---
+++ sql/2016-07-08-17-43-44.sql
@@ -0,0 +1,8 @@
+insert into app_rank (app_id, rank, rank_date, rank_type, country, category) values("50600001000017","1","20160707","Free","AO","lifestyle");
+insert into app_rank (app_id, rank, rank_date, rank_type, country, category) values("50600001003763","1","20160707","Grossing","AO","lifestyle");
+insert into app_rank (app_id, rank, rank_date, rank_type, country, category) values("50600001001081","3","20160707","Grossing","AO","lifestyle");
+insert into app_rank (app_id, rank, rank_date, rank_type, country, category) values("50600001001023","4","20160707","Free","AO","lifestyle");
+insert into app_rank (app_id, rank, rank_date, rank_type, country, category) values("50600001000017","2","20160707","Grossing","AO","lifestyle");
+insert into app_rank (app_id, rank, rank_date, rank_type, country, category) values("50600001001023","4","20160707","Grossing","AO","lifestyle");
+insert into app_rank (app_id, rank, rank_date, rank_type, country, category) values("50600001003763","2","20160707","Free","AO","lifestyle");
+insert into app_rank (app_id, rank, rank_date, rank_type, country, category) values("50600001001081","3","20160707","Free","AO","lifestyle");
